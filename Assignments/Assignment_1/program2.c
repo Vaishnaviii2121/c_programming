01fc0153d1a5b66fec26ea78c3f4f6836f3dac88
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2025-10-20 12:04:49 +0530

    Add files via upload

---
+++ Assignments/Assignment_1/program2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description : To display message 
+//  Author : Vaishnavi
+//  Date :  20/10/2025
+//
+////////////////////////////////////////////////////////////////
+
+void Display()
+{
+    int i = 0;
+    for(i = 1;i<=5;i++)
+    {
+        printf("Marvellous\n");
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
