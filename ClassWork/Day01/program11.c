4ece4a202b28eb1ccff7bed9abe5096139fd5a61
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2025-10-20 12:18:11 +0530

    Add files via upload

---
+++ ClassWork/Day01/program11.c
@@ -0,0 +1,87 @@
+/*
+ALGORITHM
+
+START
+    Accept first number as no1
+    Accept second number as no2
+    If input is negative then convert it into positive
+    Perform addition of no1 and no2
+    Display the Addition on Screen
+STOP
+
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//   Required header file
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : AdditionTwoNnumber  
+//  Description :   It is used to perform addition
+//  Input :         float ,float
+//  Output :        float
+//  Author :        Vaishnavi Bchukaka Garad
+//  Date :          09/10/2026
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+float AdditionTwoNnumber(
+                            float fNo1,                         // first input 
+                            float fNo2                          // second input
+                        )
+{
+    float fSum = 0.0f;                                          // to store the result
+
+    if(fNo1 < 0.0f)                                             //Updator         
+    {
+        fNo1 = -fNo1;
+    }
+
+    if(fNo2 < 0.0f)                                             //Updator
+    {
+        fNo2 = -fNo2;
+    }
+
+    fSum = fNo1 + fNo2;                                         //business logic 
+    return fSum;
+}       // End of AdditionTwoNnumber
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry Point Function for the Application
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()                                                      
+{
+    float fValue1 = 0.0f, fValue2 = 0.0f;                        // to accept user input
+    float fRet = 0.0f;                                           // to store result
+
+    printf("Enter first number :\n");
+    scanf("%f",&fValue1);
+
+    printf("Enter second number :\n");
+    scanf("%f",&fValue2);
+
+    fRet = AdditionTwoNnumber(fValue1,fValue2);                  // method call
+
+    printf("Addition is :%f\n",fRet);
+
+    return 0;
+}       //End of Main
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Test cases Successfully handled by the Application
+//
+//  Input1:10.5     Input2: 3.2     Output2: 13.7
+//  Input1:-10.5     Input2: 3.2     Output2: 13.7
+//  Input1:10.5     Input2: -3.2     Output2: 13.7
+//  Input1:-10.5     Input2:-3.2     Output2: 13.7
+//  Input1:10.5     Input2: 0.0     Output2: 10.5
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
