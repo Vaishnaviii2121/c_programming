1980ce16a112017da3a875b07d56e7e0c4393544
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2025-11-04 00:24:35 +0530

    Add program to multiply digits of a number

---
+++ Assignment/Assignment15/program15_4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int MultDigits(int iNo)
+{
+    int iDigit = 0, iMult = 1;
+
+    if(iNo < 0)
+        iNo = -iNo;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iMult = iMult * iDigit;
+        iNo = iNo / 10;
+    }
+
+    return iMult;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    iRet = MultDigits(iValue);
+    printf("%d", iRet);
+
+    return 0;
+}
