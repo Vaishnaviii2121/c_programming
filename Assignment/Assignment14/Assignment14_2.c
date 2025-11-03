6e23d6276b991aae4fc43ae6da0710f3cf6d0088
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2025-11-04 00:13:32 +0530

    Add CheckZero function to detect zeros in a number

---
+++ Assignment/Assignment14/Assignment14_2.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL CheckZero(int iNo)
+{
+    int iDigit = 0;
+
+    if(iNo<2)
+    {
+        iNo = -iNo;
+    }
+    while(iNo!=0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit==0)
+        {
+            return TRUE ;
+        }
+        iNo = iNo / 10;
+    }
+    return FALSE;
+}
+int main()
+{
+    int iValue =0;
+    BOOL bRet = FALSE;
+
+    printf("Enter Number : ");
+    scanf("%d",&iValue);
+
+    bRet = CheckZero(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("it contains zero \n");
+    }
+    else{
+        printf("There is no zero\n");
+    }
+
+
+}
