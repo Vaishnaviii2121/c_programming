425e85e90ef3224894884a7703d0e6319fdc976f
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2025-11-04 00:22:21 +0530

    Create program15_1.c

---
+++ Assignment/Assignment15/program15_1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int CountEven(int iNo)
+{
+    int iCnt = 0, iDigit = 0;
+
+    if(iNo < 0)
+        iNo = -iNo;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit % 2 == 0)
+            iCnt++;
+        iNo = iNo / 10;
+    }
+
+    return iCnt;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    iRet = CountEven(iValue);
+    printf("%d", iRet);
+
+    return 0;
+}
