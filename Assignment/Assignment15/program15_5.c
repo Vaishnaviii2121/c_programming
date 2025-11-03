54136cf2185365517b2b67c14aaa6ac594854665
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2025-11-04 00:25:14 +0530

    Create program15_5.c

---
+++ Assignment/Assignment15/program15_5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int CountDiff(int iNo)
+{
+    int iDigit = 0;
+    int iSumEven = 0, iSumOdd = 0;
+
+    if(iNo < 0)
+        iNo = -iNo;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit % 2 == 0)
+            iSumEven = iSumEven + iDigit;
+        else
+            iSumOdd = iSumOdd + iDigit;
+
+        iNo = iNo / 10;
+    }
+
+    return iSumEven - iSumOdd;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    iRet = CountDiff(iValue);
+    printf("%d", iRet);
+
+    return 0;
+}
