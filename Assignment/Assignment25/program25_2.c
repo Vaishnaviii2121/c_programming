4e7902cf9eeb888da191535780f4a1fb832511a6
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2025-11-19 00:05:46 +0530

    Add files via upload

---
+++ Assignment/Assignment25/program25_2.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+////////////////////////////////////////////////////////////////
+// 
+//  Function Name : DisplayDivisibleByFive
+//  Description   : Displays all elements divisible by 5
+//  Input         : Integer array, length
+//  Author        : Vaishnavi
+//  Date          : 18/11/2025
+//
+////////////////////////////////////////////////////////////////
+
+void DisplayDivisibleByFive(int Arr[], int iSize)
+{
+    int iCnt = 0;
+
+    printf("Elements divisible by 5 are:\n");
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] % 5 == 0)
+        {
+            printf("%d\n", Arr[iCnt]);
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////
+//
+// Entry point function
+//
+/////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iSize = 0, iCnt = 0;
+    int *Ptr = NULL;
+
+    printf("Enter number of elements:\n");
+    scanf("%d", &iSize);
+
+    Ptr = (int *)malloc(iSize * sizeof(int));
+    if(Ptr == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return -1;
+    }
+
+    printf("Enter elements:\n");
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        scanf("%d", &Ptr[iCnt]);
+    }
+
+    DisplayDivisibleByFive(Ptr, iSize);
+
+    free(Ptr);
+    return 0;
+}
