adc8d20f36d297cc4c285cbeac548c15121a2c54
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2025-11-24 00:00:59 +0530

    Create program29_1.c

---
+++ Assignment/Assignment29/program29_1.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description : Accept number of rows and columns from user and display Pattern
+//  Input : iRow = 3    iCol = 5
+//  Output : 1  2   3   4   
+//           5  6   7   8
+//           9  1   2   3   
+//           4  5   6   7          
+//  Author : Vaishnavi
+//  Date : 23/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int num = 1;
+
+    for(int i = 1; i <= iRow; i++)
+    {
+        for(int j = 1; j <= iCol; j++)
+        {
+            printf("%d\t", num);
+
+            num++;
+            if(num == 10)   // After 9, reset to 1
+            {
+                num = 1;
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
