4d6dc19d6b7822fe5d2c7c4ce5e30fbf9c863bd2
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2025-11-23 23:52:46 +0530

    Add files via upload

---
+++ Assignment/Assignment30/program30_2.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description : Accept number of rows and columns from user and display Pattern
+//  Input : iRow = 3    iCol = 5
+//  Output : *  *   *   *   
+//           *  *   *   #
+//           *  *   #   #
+//           *  #   #   *         
+//  Author : Vaishnavi
+//  Date : 23/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    for(int i = 1; i <= iRow; i++)
+    {
+        for(int j = 1; j <= iCol; j++)
+        {
+            if(j >= i)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
