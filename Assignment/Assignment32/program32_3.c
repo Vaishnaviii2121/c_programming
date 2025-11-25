9071ef6a5831229c6872472277ddcb555cbd12f1
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2025-11-26 00:11:13 +0530

    Add files via upload

---
+++ Assignment/Assignment32/program32_3.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description : Accepting number of rows and columns from user and display Pattern
+//  Input : iRow = 6    iCol = 6
+//  Output : 
+//                   *   *   *   *   *   *                        
+//                   *               *   *
+//                   *           *       *
+//                   *       *           *
+//                   *    *              *
+//                   *   *   *   *   *   *
+//  Author : Vaishnavi
+//  Date : 25/11/2025
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow,int iCol)
+{
+    int i, j;
+
+    if(iRow!=iCol)
+    {
+        printf("Please enter the row and column same: \n");
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+           if(i == 1 || i == iRow)        // top & bottom row
+            {
+                printf("*\t");
+            }
+            else if(j == i)               // main diagonal
+            {
+                printf("*\t");
+            }
+            else if(j == (iCol - i + 1))  // anti diagonal
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf(" \t");            // empty space inside
+            }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter a number of Rows: ");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of Columns: ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
